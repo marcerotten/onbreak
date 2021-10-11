e2269e3443237a18c0fda2b82156f2c75c2ed217
marcerotten <marce.rotten@gmail.com>
2021-10-11 18:36:43 -0300

    add login

--- onbreakbd/ClienteWPF/MainWindow.xaml.cs
+++ onbreakbd/ClienteWPF/MainWindow.xaml.cs
@@ -6,72 +6,114 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 using BibliotecaCliente;
+using System.Windows.Media;
+using System.Net.Http;
 
 namespace ClienteWPF
 {
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : MetroWindow
     {
-        List<Cliente> miCliente = new List<Cliente>();
-        
+        Dictionary<string, string> usuarios = new Dictionary<string, string>()
+        {
+            { "lucas", "lucas123" },
+            { "tomas", "tomas123" },
+            { "profesor", "profe123" }
+        };
 
         public MainWindow()
         {
             InitializeComponent();
-
         }
 
-   
-
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void TxtUsuario_GotFocus(object sender, RoutedEventArgs e)
         {
-
+            txtUsuario.Foreground = new SolidColorBrush(Colors.Gray);
         }
 
-    
-
-        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        private void TxtContraseña_GotFocus(object sender, RoutedEventArgs e)
         {
-            Window2 ventana2 = new Window2();
-            ventana2.Owner = this;
-            ventana2.ShowDialog();
+            txtContraseña.Foreground = new SolidColorBrush(Colors.Gray);
         }
 
-
-        private void MenuItem_Click_4(object sender, RoutedEventArgs e)
+        private async void BtnEntrar_Click(object sender, RoutedEventArgs e)
         {
-            Hide();
+            string usuario = txtUsuario.Text;
+            string contraseña = txtContraseña.Password;
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync($"http://localhost:8080/api/{usuario}");
+                response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode)
+                {
+                    message.Content = await response.Content.ReadAsStringAsync();
+                    Menu ventana = new Menu();
+                    ventana.Show();
+                }
+                else
+                {
+                    message.Content = $"Server error code {response.StatusCode}";
+                }
+            }
         }
 
-
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        private void TxtContraseña_LostFocus(object sender, RoutedEventArgs e)
         {
-            Window1 ventana= new Window1();
-            ventana.Show();
-            this.Close();
+            txtContraseña.Foreground = new SolidColorBrush(Colors.LightGray);
         }
 
-        private void BtnCliente_Click(object sender, RoutedEventArgs e)
+        private void TxtUsuario_LostFocus(object sender, RoutedEventArgs e)
         {
-            Window2 window = new Window2();
-            window.Show();
-            this.Close();
+            txtUsuario.Foreground = new SolidColorBrush(Colors.LightGray);
         }
 
-        private void BtnContrato_Click(object sender, RoutedEventArgs e)
+        private void highContrast()
         {
-            ListaContra window = new ListaContra();
-            window.Show();
-
+            LinearGradientBrush back = (LinearGradientBrush)fondo.BorderBrush;
+            back.GradientStops[0].Color = Colors.Green;
+            back.GradientStops[1].Color = Colors.Black;
+            lblTitulo.Foreground = new SolidColorBrush(Colors.Green);
+            lblIngreso.Foreground = new SolidColorBrush(Colors.Green);
+            lblUsuario.Foreground = new SolidColorBrush(Colors.Green);
+            lblContraseña.Foreground = new SolidColorBrush(Colors.Green);
+            fondoInterno.Background = new SolidColorBrush(Colors.Black);
+            borde.BorderBrush = new SolidColorBrush(Colors.Green);
+            BtnEntrar.Background = new SolidColorBrush(Colors.Green);
+            BtnEntrar.Foreground = new SolidColorBrush(Colors.Black);
+            //Info.highContrast = true;
         }
 
-        private void BtnCliente_Click_1(object sender, RoutedEventArgs e)
+        private void lowContrast()
         {
-            ListaCltes window = new ListaCltes();
-            window.Show();
-
+            LinearGradientBrush back = (LinearGradientBrush)fondo.BorderBrush;
+            Color miColor = Color.FromRgb(17, 144, 203);
+            back.GradientStops[0].Color = miColor;
+            back.GradientStops[1].Color = Colors.White;
+            lblTitulo.Foreground = new SolidColorBrush(miColor);
+            lblIngreso.Foreground = new SolidColorBrush(miColor);
+            lblUsuario.Foreground = new SolidColorBrush(miColor);
+            lblContraseña.Foreground = new SolidColorBrush(miColor);
+            fondoInterno.Background = new SolidColorBrush(Colors.White);
+            borde.BorderBrush = new SolidColorBrush(miColor);
+            BtnEntrar.Background = new SolidColorBrush(Colors.Gray);
+            BtnEntrar.Foreground = new SolidColorBrush(miColor);
+            //Info.highContrast = false;
         }
+
+        //private void HighContrast_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (!Info.highContrast)
+        //    {
+        //        highContrast();
+        //    }
+        //    else
+        //    {
+        //        lowContrast();
+        //    }
+        //}
     }
 }
